55ec993b9e30493675971a3006881dbe59561bdf
Rohit Jain <rohitjain@Rohits-MacBook-Pro.local>
2021-03-31 22:57:14 +0530

    Updated solution

--- amazon/verifying-an-alien-dictionary.py
+++ amazon/verifying-an-alien-dictionary.py
@@ -12,3 +12,22 @@ class Solution:
                         return False
                     break
         return True
+
+# with Lambda 
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        sorting = {e:i for i, e in enumerate(order)}
+        return words == sorted(words, key = lambda x: [sorting[c] for c in x])
+
+# With comparator
+class Solution:
+	def isAlienSorted(self, words: List[str], order: str) -> bool:
+        D = {e:i for i,e in enumerate(order)}
+        def comparator(x1, x2):
+            for i in range(min(len(x1), len(x2))):
+                if x1[i] != x2[i]:
+                    return -1 if D[x1[i]] < D[x2[i]] else 1
+            if len(x1) == len(x2):
+                return 0
+            return -1 if len(x1) < len(x2) else 1
+        return words == sorted(words, key = functools.cmp_to_key(comparator))
