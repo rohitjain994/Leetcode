8496c00f9b730cdcdb5b354e6bffdb7c6c7234b8
Rohit Jain <rohitjain@Rohits-MacBook-Pro.local>
2021-04-01 13:48:15 +0530

    Updated solution

--- amazon/subarray-sum-target-k.py
+++ amazon/subarray-sum-target-k.py
@@ -12,3 +12,21 @@ class Solution:
 
 # Complexity O(n),O(n)
 # Prefix sum approach O(n^2)
+
+#to get range
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> List[(int,int)]:
+        count,rsum = 0,0
+        mapsum = collections.defaultdict(list)
+        mapsum[0].append(1)
+        res = []
+        for i in range(len(nums)):
+            rsum+=nums[i]
+            if rsum == k:
+                res.append((0,i))
+            if rsum-k in mapsum:
+                for it in mapsum[rsum-k]:
+                    res.append((it+1,i))
+            mapsum[rsum].append(i)
+        return res
