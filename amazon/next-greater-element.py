2b5688091d0f95d7d09fa34bd102a6ec0f17fe64
Rohit Jain <rohitjain@Rohits-MacBook-Pro.local>
2021-04-07 18:08:35 +0530

    Updated solution

--- amazon/next-greater-element.py
+++ amazon/next-greater-element.py
@@ -26,3 +26,33 @@ class Solution:
             res[i%n] = -1 if len(stack)==0 else nums[stack[-1]]
             stack.append(i%n)
         return res
+
+https://leetcode.com/problems/next-greater-element-i/
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        mapper = {e:i for i,e in enumerate(nums2)}
+        stack = []
+        res = [-1]*len(nums2)
+        for i in range(len(nums2)):
+            while stack and nums2[stack[-1]]<nums2[i]:
+                res[stack[-1]] = nums2[i]
+                stack.pop()
+            stack.append(i) 
+        return [res[mapper[i]] for i in nums1]
+
+#Next greator number
+stack = []
+res = [-1]*len(nums)
+for i in range(len(nums)): 
+    while stack and nums[stack[-1]]<nums[i]:
+        res[stack[-1]] = nums[i]
+        stack.pop()
+    stack.append(i)
+#Previous greator number
+stack = [nums[0]]
+res = [-1]
+for i in range(1,len(nums)):
+    while stack and stack[-1] < nums[i]:
+        stack.pop()
+    res.append(stack[-1] if stack else -1)
+    stack.append(nums[i])
